6b3c3384b4bbbd9f02ffe3bab2df74260c16abb0
shizuka <shiiishim920@gmail.com>
2021-08-16 15:41:14 +0900

    カレンダープログラムの実装

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,53 @@
+require "date"
+require 'optparse'
+
+opt = ARGV.getopts("y:m:")
+today = Date.today
+
+# オプションの指定があれば、該当の年月を、なければ当日を代入する
+year = if opt["y"]
+        opt["y"].to_i
+       else
+        today.year
+       end
+
+month = if opt["m"]
+          opt["m"].to_i
+        else
+          today.month
+        end
+
+first_day = Date.new(year, month, 1)
+last_day = Date.new(year, month, -1)
+week_day = ["日", "月", "火", "水", "木", "金", "土"]
+
+# いつのカレンダーか表示する
+puts "#{month}月 #{year}".center(20)
+# 曜日を表示する
+puts week_day.join(" ")
+# カレンダーを表示する
+(first_day..last_day).each do |x|
+  # 初日の曜日に合わせてスペースを入れる
+  brank = "   "
+  if x.day == 1 && x.wday == 1
+    print brank
+  elsif x.day == 1 && x.wday == 2
+    print brank * 2
+  elsif x.day == 1 && x.wday == 3
+    print brank * 3
+  elsif x.day == 1 && x.wday == 4
+    print brank * 4
+  elsif x.day == 1 && x.wday == 5
+    print brank * 5
+  elsif x.day == 1 && x.wday == 6
+    print brank * 6
+  end
+
+  if x.day <= 9  # 1桁のときは、数字の左右にスペースを入れる
+    print " #{x.day.to_s} "
+  else
+    print "#{x.day.to_s} "  # 2桁のときは、数字の右にスペースを入れる
+  end
+  puts "\n" if x.saturday?  # 土曜日の後に改行を入れる
+end
+
