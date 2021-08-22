7bbd3d0adfd81164f802c44a3e812ca567a59755
shizuka <shiiishim920@gmail.com>
2021-08-22 17:10:12 +0900

    不要なコメントの削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,7 +6,6 @@ require 'optparse'
 opt = ARGV.getopts("y:m:")
 today = Date.today
 
-# オプションの指定があれば、該当の年月を、なければ当日を代入する
 year = if opt["y"]
         opt["y"].to_i
        else
@@ -23,16 +22,12 @@ first_day = Date.new(year, month, 1)
 last_day = Date.new(year, month, -1)
 week_day = ["日", "月", "火", "水", "木", "金", "土"]
 
-# いつのカレンダーか表示する
 puts "#{month}月 #{year}".center(20)
-# 曜日を表示する
 puts week_day.join(" ")
-# カレンダーを表示する
 (first_day..last_day).each do |x|
-  # 初日の曜日に合わせてスペースを入れる
   blank = "   "
   print blank *= x.wday if x.day == 1
-  print "#{x.day.to_s} ".rjust(3) # 日にちを右寄せして表示する
-  puts "\n" if x.saturday?  # 土曜日の後に改行を入れる
+  print "#{x.day.to_s} ".rjust(3)
+  puts "\n" if x.saturday?
 end
 
