2a5969515fcbb5926f1acc33c513d99b29b48ed4
shizuka <shiiishim920@gmail.com>
2021-09-04 11:37:35 +0900

    rubocopの実行&修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -19,15 +19,15 @@ frames = shots.each_slice(2).to_a
 point = 0
 frames.each_with_index do |frame, i|
   point += frame.sum
-  if i < 9 && (frame[0] + frame[1] == 10) # 加点
-    point += frames[i + 1][0]
-    if frame[0] == 10
-      point += if frames[i + 1][0] == 10
-                frames[i + 2][0]
-               else
-               frames[i + 1][1]
-               end
-    end
-  end
+  next unless i < 9 && (frame[0] + frame[1] == 10) # 加点
+
+  point += frames[i + 1][0]
+  next unless frame[0] == 10
+
+  point += if frames[i + 1][0] == 10
+             frames[i + 2][0]
+           else
+             frames[i + 1][1]
+           end
 end
 puts point
