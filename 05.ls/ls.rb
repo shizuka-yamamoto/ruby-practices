3e6d38a777b92380bd7868e2299519ecf7e9570a
shizuka <shiiishim920@gmail.com>
2021-09-06 20:56:35 +0900

    メソッドを作成し、if文をメソッド内に組み込む

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,31 +4,38 @@
 require 'optparse'
 require 'etc'
 
-options = ARGV.getopts('a', 'l', 'r')
-files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-files = files.reverse if options['r']
+def main
+  options = ARGV.getopts('a', 'l', 'r')
+  files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+  files = files.reverse if options['r']
 
-# 基準なる行数
-line_count = if (files.size % 3).zero?
-               files.size / 3
-             else
-               files.size / 3 + 1
-             end
-
-# transposeを使うために一時的な配列の作成
-temporary_array = files.each_slice(line_count).to_a
-temporary_array.each do |line|
-  next unless line.size < line_count
-
-  (line_count - line.size).times do
-    line << ' '
+  if options['l']
+    l_option(files)
+  else
+    standard_output(files)
   end
 end
 
-filename_length = files.map(&:length)
-max_filename_length = filename_length.max
+def standard_output(files)
+   # 基準なる行数
+  line_count = if (files.size % 3).zero?
+                 files.size / 3
+               else
+                 files.size / 3 + 1
+               end
 
-unless options['l']
+   # transposeを使うために一時的な配列の作成
+  temporary_array = files.each_slice(line_count).to_a
+  temporary_array.each do |line|
+    next unless line.size < line_count
+
+    (line_count - line.size).times do
+      line << ' '
+    end
+  end
+
+  max_filename_length = files.map(&:length).max
+
   temporary_array.transpose.each do |array|
     array.each do |file|
       print file.ljust(max_filename_length + 2)
@@ -37,31 +44,7 @@ unless options['l']
   end
 end
 
-if options['l']
-  def file_type(type)
-    {
-      '10' => '-',
-      '04' => 'd',
-      '12' => 'l'
-    }[type]
-  end
-
-  def file_mode(number_mode)
-    to_symbol_mode = {
-      '0' => '---',
-      '1' => '--x',
-      '2' => 'r--',
-      '3' => '-w-',
-      '4' => '-wx',
-      '5' => 'r-x',
-      '6' => 'rw-',
-      '7' => 'rwx'
-    }
-    number_mode.map do |number|
-      to_symbol_mode[number]
-    end
-  end
-
+def l_option(files)
   file_blocks = files.map { |file| File.lstat(file).blocks }
   puts "total #{file_blocks.sum}"
 
@@ -79,3 +62,29 @@ if options['l']
     puts "#{file_type}#{file_mode} #{number_of_links.to_s.rjust(3)} #{owner_name} #{group_name} #{file_size.to_s.rjust(4)} #{time_stamp} #{file_name}"
   end
 end
+
+def file_type(type)
+  {
+    '10' => '-',
+    '04' => 'd',
+    '12' => 'l'
+  }[type]
+end
+
+def file_mode(number_mode)
+  to_symbol_mode = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => 'r--',
+    '3' => '-w-',
+    '4' => '-wx',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }
+  number_mode.map do |number|
+    to_symbol_mode[number]
+  end
+end
+
+main
